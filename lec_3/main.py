a9ab8d31ac5e6b85354dffe960228dd46433bcf0
Elen Arshakyan <elenarshakyan3@gmail.com>
2023-10-31 13:53:54 +0400

    lec_3 changed and added

--- lec_3/main.py
+++ lec_3/main.py
@@ -1,18 +1,28 @@
-def main():
-    print("Simple Calculator")
 
-    while True:
-        try:
-            expression = input("Enter an expression (e.g., 2 + 3): ")
+def evaluate_expression(expression):
+    try:
+        for operator in ['+', '-', '*', '/']:
+            if operator in expression:
+                operand1, operand2 = expression.split(operator)
+                operand1 = float(operand1)
+                operand2 = float(operand2)
 
-            if expression.lower() == "exit":
-                print("Goodbye!")
-                break
+                if operator == '+':
+                    result = operand1 + operand2
+                elif operator == '-':
+                    result = operand1 - operand2
+                elif operator == '*':
+                    result = operand1 * operand2
+                elif operator == '/':
+                    if operand2 == 0:
+                        return "Error: Division by zero"
+                    result = operand1 / operand2
 
-            result = eval(expression)
-            print("Result: " + str(result))
-        except Exception as e:
-            print("Error: " + str(e))
+                return f"Result: {result}"
 
-if __name__ == "__main__":
-    main()
+        return "Error: Invalid operator"
+
+    except ValueError:
+        return "Error: Invalid input format"
+
+print(evaluate_expression(input("Enter a mathematical expression (e.g., '2+3'): ")))
